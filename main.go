a09d4c64a5465f4c7289f2cefe3859f172cf353f
k-jingyang <k.jingyang@gmail.com>
2020-04-18 10:21:53 +0800

    Proof of concept code for handling stored jobs

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -15,43 +20,45 @@ type BusArrivalInformation struct {
 }
 
 var outgoingMessages chan tgbotapi.MessageConfig
+var bot tgbotapi.BotAPI
 
 func init() {
 	outgoingMessages = make(chan tgbotapi.MessageConfig)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	botToken := os.Getenv("TELEGRAM_API_TOKEN")
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 }
 
 func main() {
-	botToken := os.Getenv("TELEGRAM_API_TOKEN")
-	bot, err := tgbotapi.NewBotAPI(botToken)
+	go handleIncomingMessages()
 
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bot.Debug = true
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Panic(err)
-	}
-	go handleIncomingMessages(updates)
-
-	busArrivalInformation := BusArrivalInformation{BusStopName: "Opp 628", BusServiceNo: 506, NextBusMinutes: 10}
-	textMessage := constructBusArrivalMessage(busArrivalInformation)
-	go sendOutgoingMessage(, textMessage)
+	go handleStoredJobs()
 
 	for outgoingMesage := range outgoingMessages {
 		bot.Send(outgoingMesage)
+		log.Println("SENT")
 	}
-
 }
 
-func handleIncomingMessages(msgChannel tgbotapi.UpdatesChannel) {
-	for update := range msgChannel {
+func handleIncomingMessages() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updateMsgChannel, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for update := range updateMsgChannel {
 		if update.Message == nil {
 			continue
 		}
@@ -64,11 +71,55 @@ func handleIncomingMessages(msgChannel tgbotapi.UpdatesChannel) {
 	}
 }
 
+func handleStoredJobs() {
+	fetchBusArrivalInformation("43411", "")
+	busArrivalInformation := BusArrivalInformation{BusStopName: "Opp 628", BusServiceNo: 506, NextBusMinutes: 10}
+	textMessage := constructBusArrivalMessage(busArrivalInformation)
+
+	chatID, err := strconv.ParseInt(os.Getenv("CHAT_ID"), 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sendOutgoingMessage(chatID, textMessage)
+}
+
+func fetchBusArrivalInformation(busStopCode string, busServiceNo string) BusArrivalInformation {
+	resp, err := http.DefaultClient.Do(buildBusArrivalAPIRequest(busStopCode, busServiceNo))
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println(string(body))
+	return BusArrivalInformation{}
+}
+
+func buildBusArrivalAPIRequest(busStopCode string, busServiceNo string) *http.Request {
+	params := url.Values{}
+	params.Add("BusStopCode", busStopCode)
+	params.Add("ServiceNo", busServiceNo)
+
+	url := url.URL{
+		Scheme:   "http",
+		Host:     "datamall2.mytransport.sg",
+		Path:     "ltaodataservice/BusArrivalv2",
+		RawQuery: params.Encode(),
+	}
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ltaToken := os.Getenv("LTA_API_TOKEN")
+	req.Header.Add("AccountKey", ltaToken)
+	return req
+}
+
 func constructBusArrivalMessage(busArrivalInformation BusArrivalInformation) string {
 	return busArrivalInformation.BusStopName + " | " + strconv.Itoa(busArrivalInformation.BusServiceNo) + " in " + strconv.Itoa(busArrivalInformation.NextBusMinutes) + " minutes"
 }
 
-func sendOutgoingMessage(chatId int64, textMessage string) {
-	messageToSend := tgbotapi.NewMessage(chatId, textMessage)
+func sendOutgoingMessage(chatID int64, textMessage string) {
+	messageToSend := tgbotapi.NewMessage(chatID, textMessage)
 	outgoingMessages <- messageToSend
 }
