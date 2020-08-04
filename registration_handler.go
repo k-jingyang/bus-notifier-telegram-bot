0d410c807aa0b952223484d57c9b1f2eecd48a51
k-jingyang <k.jingyang@gmail.com>
2020-08-04 16:18:26 +0800

    Moved stored_user_state out of registration_handler

--- registration_handler.go
+++ registration_handler.go
@@ -1,49 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/boltdb/bolt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
-
-// UserState stores the state of user's registration
-// 0 (nothing)
-// 1 (user asked about bus number)
-// 2 (user asked about bus stop number)
-// 3 (user asked about which days, can self loop)
-// 4 (user asked about what time)
-// 5 (user asked which alarm to delete)
-type UserState struct {
-	State int
-	BusInfoJob
-	SelectedDays map[time.Weekday]bool
-}
-
-func (userState *UserState) toggleDay(day time.Weekday) {
-	userState.SelectedDays[day] = !userState.SelectedDays[day]
-}
-
-func (userState *UserState) getSelectedDays() []time.Weekday {
-	selectedDays := []time.Weekday{}
-	for k, v := range userState.SelectedDays {
-		if v {
-			selectedDays = append(selectedDays, k)
-		}
-	}
-	sort.Slice(selectedDays, func(i, j int) bool {
-		return int(selectedDays[i]) < int(selectedDays[j])
-	})
-
-	return selectedDays
-}
 
 type registrationReply struct {
 	replyMessage     tgbotapi.Chattable
@@ -128,15 +93,15 @@ func handleRegistration(update tgbotapi.Update) registrationReply {
 			dayInt, _ := strconv.Atoi(update.CallbackQuery.Data)
 			// If user doesn't click on Done, store day
 			if dayInt != -1 {
-				storedUserState.toggleDay(time.Weekday(dayInt))
+				storedUserState.ToggleDay(time.Weekday(dayInt))
 				SaveUserState(chatID, *storedUserState)
 
 				stringBuilder := strings.Builder{}
 				stringBuilder.WriteString("Which days? \nSelected: ")
-				if len(storedUserState.getSelectedDays()) == 0 {
+				if len(storedUserState.GetSelectedDays()) == 0 {
 					stringBuilder.WriteString("None")
 				} else {
-					selectedDays := storedUserState.getSelectedDays()
+					selectedDays := storedUserState.GetSelectedDays()
 					stringBuilder.WriteString(joinDaysString(selectedDays))
 				}
 				stringBuilder.WriteString("\nStop me with /exit")
@@ -168,7 +133,7 @@ func handleRegistration(update tgbotapi.Update) registrationReply {
 			return registrationReply{replyMessage: reply}
 		}
 		storedUserState.ScheduledTime = ScheduledTime{Hour: hour, Minute: minute}
-		for _, day := range storedUserState.getSelectedDays() {
+		for _, day := range storedUserState.GetSelectedDays() {
 			dailyBusInfoJob := storedUserState.BusInfoJob
 			dailyBusInfoJob.Weekday = day
 			storedJobDB.StoreJob(dailyBusInfoJob)
@@ -180,7 +145,7 @@ func handleRegistration(update tgbotapi.Update) registrationReply {
 		replyMessage := fmt.Sprintf("You will be reminded for bus %s at bus stop %s every %s %02d:%02d",
 			storedUserState.BusServiceNo,
 			storedUserState.BusStopCode,
-			joinDaysString(storedUserState.getSelectedDays()),
+			joinDaysString(storedUserState.GetSelectedDays()),
 			storedUserState.ScheduledTime.Hour,
 			storedUserState.ScheduledTime.Minute)
 		reply := tgbotapi.NewMessage(chatID, replyMessage)
@@ -214,82 +179,6 @@ func handleRegistration(update tgbotapi.Update) registrationReply {
 
 	log.Fatalln("Unhandled state reached")
 	return registrationReply{replyMessage: tgbotapi.NewMessage(chatID, "Unexpected error has occured")}
-}
-
-// GetUserState retrieves the stored user state
-func GetUserState(chatID int64) *UserState {
-	key := []byte(strconv.FormatInt(chatID, 10))
-	var storedUserState UserState
-
-	db, err := bolt.Open("user_state.db", 0600, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
-
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("states"))
-		if b == nil {
-			return errors.New("Bucket does not exist")
-		}
-		storedValue := b.Get(key)
-		if storedValue == nil {
-			return errors.New("Key does not exist")
-		}
-		json.Unmarshal(storedValue, &storedUserState)
-		return nil
-	})
-
-	// If there's no matching record in database
-	if err != nil {
-		return nil
-	}
-	return &storedUserState
-}
-
-// SaveUserState saves the user state, holding information about the stage of registration that the user is at
-func SaveUserState(chatID int64, userState UserState) {
-	userState.ChatID = chatID
-	log.Println("Saving user interaction state:", userState)
-
-	key := []byte(strconv.FormatInt(chatID, 10))
-
-	db, err := bolt.Open("user_state.db", 0600, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
-
-	db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte("states"))
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		encUserState, err := json.Marshal(userState)
-		b.Put(key, encUserState)
-		return nil
-	})
-}
-
-// DeleteUserState deletes the saved user state
-func DeleteUserState(chatID int64) {
-	key := []byte(strconv.FormatInt(chatID, 10))
-
-	db, err := bolt.Open("user_state.db", 0600, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
-
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("states"))
-		if b == nil {
-			log.Fatalln("Bucket should exist but doesn't exist")
-		}
-		b.Delete(key)
-		return nil
-	})
 }
 
 func buildLocationKeyboard() tgbotapi.ReplyKeyboardMarkup {
---
+++ stored_user_state.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+// UserState stores the state of user's registration
+// 0 (nothing)
+// 1 (user asked about bus number)
+// 2 (user asked about bus stop number)
+// 3 (user asked about which days, can self loop)
+// 4 (user asked about what time)
+// 5 (user asked which alarm to delete)
+type UserState struct {
+	State int
+	BusInfoJob
+	SelectedDays map[time.Weekday]bool
+}
+
+// ToggleDay toggles the truthy selection of the day
+func (userState *UserState) ToggleDay(day time.Weekday) {
+	userState.SelectedDays[day] = !userState.SelectedDays[day]
+}
+
+// GetSelectedDays gets a list of selected days
+func (userState *UserState) GetSelectedDays() []time.Weekday {
+	selectedDays := []time.Weekday{}
+	for k, v := range userState.SelectedDays {
+		if v {
+			selectedDays = append(selectedDays, k)
+		}
+	}
+	sort.Slice(selectedDays, func(i, j int) bool {
+		return int(selectedDays[i]) < int(selectedDays[j])
+	})
+
+	return selectedDays
+}
+
+// GetUserState retrieves the stored user state
+func GetUserState(chatID int64) *UserState {
+	key := []byte(strconv.FormatInt(chatID, 10))
+	var storedUserState UserState
+
+	db, err := bolt.Open("user_state.db", 0600, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("states"))
+		if b == nil {
+			return errors.New("Bucket does not exist")
+		}
+		storedValue := b.Get(key)
+		if storedValue == nil {
+			return errors.New("Key does not exist")
+		}
+		json.Unmarshal(storedValue, &storedUserState)
+		return nil
+	})
+
+	// If there's no matching record in database
+	if err != nil {
+		return nil
+	}
+	return &storedUserState
+}
+
+// SaveUserState saves the user state, holding information about the stage of registration that the user is at
+func SaveUserState(chatID int64, userState UserState) {
+	userState.ChatID = chatID
+	log.Println("Saving user interaction state:", userState)
+
+	key := []byte(strconv.FormatInt(chatID, 10))
+
+	db, err := bolt.Open("user_state.db", 0600, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("states"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		encUserState, err := json.Marshal(userState)
+		b.Put(key, encUserState)
+		return nil
+	})
+}
+
+// DeleteUserState deletes the saved user state
+func DeleteUserState(chatID int64) {
+	key := []byte(strconv.FormatInt(chatID, 10))
+
+	db, err := bolt.Open("user_state.db", 0600, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("states"))
+		if b == nil {
+			log.Fatalln("Bucket should exist but doesn't exist")
+		}
+		b.Delete(key)
+		return nil
+	})
+}
