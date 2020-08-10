659d18ec5c8ce25f90e7090332d8899e4cc614ab
k-jingyang <k.jingyang@gmail.com>
2020-08-10 18:18:27 +0800

    Updated bus_services.txt and bootstrapping script

--- refdata/boostrap_reference_data.sh
+++ refdata/boostrap_reference_data.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 LTA_API_TOKEN=$(grep LTA_API_TOKEN ../.env | cut -d '=' -f 2-)
 
-curl -H "AccountKey: $LTA_API_TOKEN" http://datamall2.mytransport.sg/ltaodataservice/BusServices \
+curl -H "AccountKey: $LTA_API_TOKEN" 'http://datamall2.mytransport.sg/ltaodataservice/BusServices' \
 | jq -r .value[].ServiceNo \
 | uniq > bus_services.txt
 
 
-go run data_downloader.go
+curl -H "AccountKey: $LTA_API_TOKEN" 'http://datamall2.mytransport.sg/ltaodataservice/BusServices?$skip=500' \
+| jq -r .value[].ServiceNo \
+| uniq >> bus_services.txt
+
+cd refdatadownloader && go run reference_data_downloader.go
